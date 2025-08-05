b6f2a6a81c1da157e3bac57ebd67fc1b0ba8ff34
Liza Kiseleva <kisel.crew@mail.ru>
2025-08-05 17:13:19 +0700

    readme&ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,1 @@
 export * from './product';
-<<<<<<< HEAD
-=======
-export * from './basket';
-export * from './order';
-export * from './api';
-export * from './views';
-export * from './events';
->>>>>>> c0d54a099703cef532cb7e960f35190e15b29e9d
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,6 @@ import { EventEmitter } from '../components/base/events';
 export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
 export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
 
-<<<<<<< HEAD
 export const settings = {};
 
 export const globalEventEmitter = new EventEmitter();
@@ -29,52 +28,3 @@ export const EVENTS = {
 	ORDER_SUBMITTED: 'ORDER_SUBMITTED',
 	ORDER_SUBMIT_FAILED: 'ORDER_SUBMIT_FAILED',
 } as const;
-=======
-// Централизованный объект для всех селекторов и id шаблонов
-export const settings = {
-	modal: {
-		container: '#modal-container',
-		content: '.modal__content',
-		close: '.modal__close',
-	},
-	page: {
-		wrapper: '.page__wrapper',
-		catalog: '.gallery',
-		basket: '.header__basket',
-		basketCounter: '.header__basket-counter',
-	},
-	card: {
-		title: '.card__title',
-		price: '.card__price',
-		image: '.card__image',
-		category: '.card__category',
-		description: '.card__text',
-		button: '.card__button',
-		deleteButton: '.basket__item-delete',
-		index: '.basket__item-index',
-	},
-	basket: {
-		list: '.basket__list',
-		price: '.basket__price',
-		button: '.basket__button',
-	},
-	form: {
-		submit: 'button[type=submit]',
-		errors: '.form__errors',
-		input: '.form__input',
-	},
-	success: {
-		close: '.order-success__close',
-		total: '.order-success__description',
-	},
-	templates: {
-		cardCatalog: '#card-catalog',
-		cardPreview: '#card-preview',
-		cardBasket: '#card-basket',
-		basket: '#basket',
-		order: '#order',
-		contacts: '#contacts',
-		success: '#success',
-	},
-};
->>>>>>> c0d54a099703cef532cb7e960f35190e15b29e9d
